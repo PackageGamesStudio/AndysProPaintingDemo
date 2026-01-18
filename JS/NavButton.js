3afb0b48b354bb089d5834abe25b4fe771564265
PackageGamesStudios <91870557+PackageGamesStudio@users.noreply.github.com>
2026-01-18 15:50:35 -0700

    Delete JS directory

--- JS/NavButton.js
+++
@@ -1,4 +0,0 @@
-const navButton = document.querySelector('#nav-burger-button');
-const menu = document.querySelector('#nav-menu');
-
-navButton.addEventListener('click', ()=>{menu.classList.toggle('activate')});
